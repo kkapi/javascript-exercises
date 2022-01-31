b6c953cc106ee7200880c5faf0baa2537705a90c
kkapi <hZG2fik_j8@mail.ru>
2022-01-31 21:20:24 +0300

    done repearString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringRepeat, timesRepeat) {
+    let finalString = '';
 
+    if (timesRepeat === 0) return '';
+
+    if (timesRepeat === -1) return 'ERROR';
+
+    if (timesRepeat === '') return '';
+
+    for (let i = 0; i < timesRepeat; i++) {
+        finalString += stringRepeat;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
