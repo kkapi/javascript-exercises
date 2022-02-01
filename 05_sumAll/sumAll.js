9e403de53078a21d3f35430f19c8539e23071032
kkapi <hZG2fik_j8@mail.ru>
2022-02-02 01:16:56 +0300

    done sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(...numbers) {
+    const array = [...numbers].sort();
+    for(item of array) {
+        if (item < 0 || typeof(item) != "number") {
+            return "ERROR";
+        }
+    }
+    return ((array[0] + array[1]) * (array[1] - array[0] + 1) / 2);
 };
 
 // Do not edit below this line
