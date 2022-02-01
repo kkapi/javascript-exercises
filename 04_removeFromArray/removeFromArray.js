dd6b957ad424db90ed9dbd20436802f2dcee3ba6
kkapi <hZG2fik_j8@mail.ru>
2022-02-02 00:40:03 +0300

    done removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...extraValues) {
+    const result = [];
+    const values = Array.from(extraValues);
+    for (let i = 0; i < array.length; i++){
+        let flag = true;
+        for (let j = 0; j < values.length; j++) {
+            if (array[i] === values[j]) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            result.push(array[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
